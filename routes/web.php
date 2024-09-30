db9128cbb9fe57042a9bdba76036dcb4b87b97d3
unknown <laiquan163@gmail.com>
2024-09-30 15:28:25 +0700

    fix 8.2 test error

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 
-Route::get('/', function () {
-    $default = 1;
-    return redirect()->route('store', ['id' => $default]);
-})->name('home');
+Route::get('/', [ShopController::class, 'storeMenu'])->defaults('id', 1)->name('home');
 
 Route::get('store/{id}', [ShopController::class, 'storeMenu'])->name('store');
 Route::get('/filter', [ShopController::class, 'filter'])->name('filter');
